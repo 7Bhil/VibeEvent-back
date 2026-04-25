b8fb1e5d322ed7387d66bbf83068820dd2742727
7Bhil <7bhilal.chitou7@gmail.com>
2026-04-25 22:53:07 +0100

    feat: populate organization in auth middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ export const protect = async (req, res, next) => {
         try {
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            const user = await User.findById(decoded.id).select('-password');
+            const user = await User.findById(decoded.id).select('-password').populate('organization');
             
             if (user) {
                 // Check if user is blocked
